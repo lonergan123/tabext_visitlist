994a7c68a657a45b290171cb36e53ade57ff798f
lonergan123 <lonergan123@hotmail.com>
2019-06-10 15:26:24 -0600

    added image

--- EDVisitList.js
+++ EDVisitList.js
@@ -132,17 +132,14 @@
                     }
 
                     return
-
                     '<div class="container">' +
                       '<div class="row">' +
                         '<div class="col-sm">' +
-                          One of three columns
+                          d.PRESENTING_COMPLAINT +
                         '</div>' +
                         '<div class="col-sm">' +
-                          One of three columns
                         '</div>' +
                         '<div class="col-sm">' +
-                          One of three columns
                         '</div>' +
                       '</div>' +
                     '</div>'
